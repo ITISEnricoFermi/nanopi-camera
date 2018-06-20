5313d37437b42c0f7349aa6263486b2a04f72e28
n4y0n <e.montada0@gmail.com>
2018-06-20 18:14:36 +0100

    ...

---
+++ main.js
@@ -0,0 +1,14 @@
+const ffmpeg = require('ffmpeg')
+
+try {
+	new ffmpeg('./your_movie.avi', function (err, video) {
+		if (!err) {
+			console.log('The video is ready to be processed');
+		} else {
+			console.log('Error: ' + err);
+		}
+	});
+} catch (e) {
+	console.log(e.code);
+	console.log(e.msg);
+}
