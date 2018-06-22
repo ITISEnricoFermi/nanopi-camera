d0e0a0befaa3e78e4f6f5e0379881898a61ba58d
n4y0n <e.montada0@gmail.com>
2018-06-22 19:05:01 +0100

    is Running getter

--- Camera.js
+++ Camera.js
@@ -1,6 +1,6 @@
-const MjpegCamera = require('mjpeg-camera')
-const FileOnWrite = require('file-on-write')
-const mkdirp = require("mkdirp")
+const MjpegCamera = require("mjpeg-camera");
+const FileOnWrite = require("file-on-write");
+const mkdirp = require("mkdirp");
 
 class Camera {
   /**
@@ -8,68 +8,69 @@ class Camera {
    * @param {{streamUrl:string, rootdir:string, cameraName:string}} opts
    */
   constructor(opts) {
-    this.opts = Object.assign({}, opts)
-    this.running = false
-    this._restart()
+    this.opts = Object.assign({}, opts);
+    this.running = false;
+    this._restart();
   }
 
   _restart(newOpts) {
     if (newOpts) {
-      this.opts = newOpts
+      this.opts = newOpts;
     }
 
     if ("rootdir" in this.opts) {
-      mkdirp(this.opts.rootdir)
+      mkdirp(this.opts.rootdir);
     }
 
-    this.streamUrl = this.opts.streamUrl
-    this.name = this.opts.cameraName || "camera" //prefix for each frame (helps processing)
+    this.streamUrl = this.opts.streamUrl;
+    this.name = this.opts.cameraName || "camera"; //prefix for each frame (helps processing)
 
-    let videoFolderName = (this.opts.rootdir || "./") + (new Date()).getTime().toString()
+    let videoFolderName =
+      (this.opts.rootdir || "./") + new Date().getTime().toString();
 
     this.motionWriter = new FileOnWrite({
       path: videoFolderName, // Unique folder for each video
-      filename: function (frame) {
-        return frame.name + '-' + frame.time
+      filename: function(frame) {
+        return frame.name + "-" + frame.time;
       },
       // We need to pull the jpeg out of the frame object
-      transform: function (frame) {
-        return frame.data
+      transform: function(frame) {
+        return frame.data;
       },
-      ext: '.jpg'
-    })
+      ext: ".jpg"
+    });
 
-    this.camera = null
+    this.camera = null;
 
-    this._init()
+    this._init();
   }
 
   _init() {
     this.camera = new MjpegCamera({
       name: this.name,
-      url: this.streamUrl || 'http://127.0.0.1:8080/?action=stream',
-    })
-    this.camera.pipe(this.motionWriter)
-    this.camera.start()
-    this.running = true
+      url: this.streamUrl || "http://127.0.0.1:8080/?action=stream"
+    });
+    this.camera.pipe(this.motionWriter);
+    this.camera.start();
+    this.running = true;
   }
 
-  get isRunning() {
-    return this.running
+  isRunning() {
+    return this.running;
   }
 
   start() {
-    this.stop()
-    this._init()
+    this.stop();
+    this._init();
   }
 
   stop() {
     if (this.camera) {
-      this.camera.stop()
-      this.running = false
-      this.camera = null
+      this.camera.stop();
+      this.running = false;
+      this.camera = null;
     }
   }
 }
 
-module.exports = Camera
+module.exports = Camera;
